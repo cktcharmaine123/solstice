f86ba9054ebda12aa7fd873edb858c4f3ead5502
cktcharmaine123 <cktcharmaine@gmail.com>
2026-09-07 22:11:43 +0800

    Fix layout and map spacing on home

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -371,7 +371,7 @@ export const HomeScreen = (): JSX.Element => {
   }, [signalMapReady]);
 
   return (
-    <main className="app-container mx-auto flex min-h-[100dvh] w-full flex-col overflow-x-hidden bg-white">
+    <main className="app-container mx-auto flex h-[100dvh] w-full flex-col overflow-hidden bg-white">
       <section className="px-[23px] pt-[84px]" aria-label="Location search">
         <div className="flex h-10 items-center gap-3 rounded-lg bg-neutral-100 px-3 py-2">
           <SearchIcon
@@ -435,7 +435,7 @@ export const HomeScreen = (): JSX.Element => {
           ))}
         </div>
       </section>
-      <figure className="relative mx-px mt-[88px] aspect-square w-[calc(100%-2px)] overflow-hidden md:mt-[88px] md:aspect-auto md:h-[600px]">
+      <figure className="relative mx-px mt-4 min-h-0 flex-1 w-[calc(100%-2px)] overflow-hidden">
         <MapContainer
           center={DEFAULT_CENTER}
           zoom={DEFAULT_ZOOM}
