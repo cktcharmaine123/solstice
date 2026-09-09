ab2c5e4ad9ea2f54bce5cc233cd9f728c599cea0
cktcharmaine123 <cktcharmaine@gmail.com>
2026-09-09 11:31:36 +0800

    Improve cache invalidation and app startup

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,8 @@
 declare global {
   interface Window {
     __hideAppLoading?: () => void;
+    __appBooted?: boolean;
+    __recoveryTimer?: ReturnType<typeof setTimeout>;
     __fallbackTimer?: ReturnType<typeof setTimeout>;
   }
 }
